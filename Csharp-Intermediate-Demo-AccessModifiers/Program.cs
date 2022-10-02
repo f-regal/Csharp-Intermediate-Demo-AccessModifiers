58efcc60b4c2f6f6ba039f6e5b39167d9a681773
Faysal Regal <faysalregal@gmail.com>
2022-10-02 18:39:54 +0100

    first commit

---
+++ Csharp-Intermediate-Demo-AccessModifiers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_Intermediate_Demo_AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
